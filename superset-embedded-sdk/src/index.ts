e6774c9aa084012c5a8d94c1a3caa59af04fac6c
gdagkalis <gdagkalis@welcomepickups.com>
2024-01-11 20:57:23 +0200

    changed index.ts to include extraUrlParams

--- superset-embedded-sdk/src/index.ts
+++ superset-embedded-sdk/src/index.ts
@@ -42,6 +42,7 @@ export type UiConfigType = {
     visible?: boolean
     expanded?: boolean
   }
+  urlParams?: {[key: string]: any}
 }
 
 export type EmbedDashboardParams = {
@@ -121,6 +122,12 @@ export async function embedDashboard({
           .map(key => DASHBOARD_UI_FILTER_CONFIG_URL_PARAM_KEY[key] + '=' + filterConfig[key]).join('&')
         : ""
 
+      let extraUrlParams = new URLSearchParams(dashboardUiConfig?.urlParams || window.location.search).toString()
+      if (!!extraUrlParams) extraUrlParams = "&" + extraUrlParams
+      if (debug) {
+        console.debug(`[superset-embedded-sdk][extraUrlParams]`, extraUrlParams);
+      }
+
       // set up the iframe's sandbox configuration
       iframe.sandbox.add("allow-same-origin"); // needed for postMessage to work
       iframe.sandbox.add("allow-scripts"); // obviously the iframe needs scripts
@@ -153,7 +160,7 @@ export async function embedDashboard({
         resolve(new Switchboard({ port: ourPort, name: 'superset-embedded-sdk', debug }));
       });
 
-      iframe.src = `${supersetDomain}/embedded/${id}${dashboardConfig}${filterConfigUrlParams}`;
+      iframe.src = `${supersetDomain}/embedded/${id}${dashboardConfig}${filterConfigUrlParams}${extraUrlParams}`;
       //@ts-ignore
       mountPoint.replaceChildren(iframe);
       log('placed the iframe')
